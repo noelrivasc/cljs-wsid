33b8f8e73b60ba097f5da7665b79c64db83c13a0
Noel Rivas <hola@noelr.dev>
2025-03-09 21:54:21 -0600

    Move form data to :transient element of db. Experimenting with keeping transitory data needed for the program, and the data product, separate in db.

--- src/wsid/db.cljs
+++ src/wsid/db.cljs
@@ -29,7 +29,12 @@
 
 (def default-db
   {
+   ; Information that is used for procedures but that is
+   ; not the resulting data that is the goal of the program
+   :transient {
+               :factor-edit-defaults nil
+               :factor-active nil
+   }
    :factors {
-             :current nil
              :all []
    }})
