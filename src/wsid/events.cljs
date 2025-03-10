4c32941a5440042f72193ffd1c72dadbe5c9ec0f
Noel Rivas <hola@noelr.dev>
2025-03-09 21:56:38 -0600

    Refined events for factor creation and update.

--- src/wsid/events.cljs
+++ src/wsid/events.cljs
@@ -2,20 +2,21 @@
   (:require
    [re-frame.core :as re-frame]
    [wsid.db :as db]
-   [day8.re-frame.tracing :refer-macros [fn-traced]]
+   ; [day8.re-frame.tracing :refer-macros [fn-traced]] ; TODO reimplement the fn-traced and figure out what it does and how to fix the macro errors in the editor
    ))
 
 (re-frame/reg-event-db
  ::initialize-db
- (fn-traced [_ _]
+ (fn [_ _]
    db/default-db))
 
 (re-frame/reg-event-db
  :factor-create
- (fn-traced [db _] 
+ (fn [db _]    
    (let 
-    [current-factor 
-     (if (nil? (get-in db [:factors :current]))
+    [active-factor-path [:transient :factor-active]
+     active-factor 
+     (if (nil? (get-in db active-factor-path))
       {
        :id ""
        :title ""
@@ -24,12 +25,12 @@
        :max 10
        :weight 0
        }
-      (get-in db [:factors :current]))]
-     (assoc-in db [:factors :current] current-factor)
-     )))
+      (get-in db [:transient :factor-active]))]
+    (-> db
+        (assoc-in [:transient :factor-edit-defaults] active-factor)
+        (assoc-in [:transient :factor-active] active-factor)))))
 
 (re-frame/reg-event-db
  :factor-active-update
  (fn [db [_ property value]]
-   (js/console.log "We got a new thing: " property " " value)
-   db))
+   (assoc-in db [:transient :factor-active (keyword property)] value)))
