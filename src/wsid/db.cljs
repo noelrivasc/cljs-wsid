87fbd67ca06b568d0300b5b00d7ce6e16d25e714
Noel Rivas <hola@noelr.dev>
2025-03-13 23:08:48 -0600

    Remove example from db

--- src/wsid/db.cljs
+++ src/wsid/db.cljs
@@ -1,32 +1,5 @@
 (ns wsid.db)
 
-#_(def mock-factors [
-             {
-              :id "99699f15-3dc7-4f48-b5c2-827092c26e9f"
-              :title "Factor A - only positive, high weight"
-              :description "Description of the factor A"
-              :min 0
-              :max 10
-              :weight 0
-             }
-             {
-              :id "02ff1662-21b0-4dc4-894b-ffdaae42b5da"
-              :title "Factor B - both ways, mild weight"
-              :description "Description of the factor B"
-              :min 0
-              :max 10
-              :weight 0
-             }
-             {
-              :id "7e55b826-5101-4c64-a9e9-0993e35c7d3c"
-              :title "Factor C - only negative, high weight"
-              :description "Description of the factor C"
-              :min -10
-              :max 0
-              :weight 0
-             }
-   ])
-
 (def default-db
   {
    ; Information that is used for procedures but that is
