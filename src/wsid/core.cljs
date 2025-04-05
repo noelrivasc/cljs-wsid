6c3757f382e0e8d4e9be9f94d96a8bf84ebe0b75
Noel Rivas <hola@noelr.dev>
2025-04-05 17:25:26 -0600

    Set the global theme to the slate stub

--- src/wsid/core.cljs
+++ src/wsid/core.cljs
@@ -4,13 +4,19 @@
    [re-frame.core :as re-frame]
    [wsid.events :as events]
    [wsid.views.main :as views]
-   [wsid.config :as config]))
-
-
+   [wsid.config :as config]
+   [wsid.util.theming :refer [set-theme]]
+   [wsid.views.themes.slate :refer [theme]]))
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
+
+; This sets the theme that the whole application
+; will use (the Tailwind classes that will be applied
+; to every HTML element) when wsid.util.theming/apply-theme
+; is called.
+(set-theme theme)
 
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
