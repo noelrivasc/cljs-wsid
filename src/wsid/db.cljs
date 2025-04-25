a6aa4bf7745078065bfa2c6d856f4c83822a754a
Noel Rivas <hola@noelr.dev>
2025-04-24 18:20:37 -0600

    Complete db spec and add scenarios (#2)
    * Complete the spec to cover the existing app-db
    * Add spec for scenarios

--- src/wsid/db.cljs
+++ src/wsid/db.cljs
@@ -1,6 +1,6 @@
 (ns wsid.db
   (:require
-    [clojure.spec.alpha :as s]))
+   [clojure.spec.alpha :as s]))
 
 ; -- FACTORS ------------------------
 (s/def :factor/id (s/or :empty (s/and string? #(= 0 (count %)))
@@ -19,11 +19,38 @@
 ;  :weight 0
 ;  }
 (s/def ::factor (s/keys :req-un [:factor/title :factor/min :factor/max]
-                       :opt-un [:factor/id :factor/description]))
-(s/def ::factor-list (s/coll-of ::factor :kind vector?))
+                        :opt-un [:factor/id :factor/description]))
+(s/def :factors/all (s/coll-of ::factor :kind vector?))
 
 ; -- SCENARIOS ----------------------
-          
+(s/def :scenario/name string?)
+(s/def :scenario/description string?)
+(s/def :scenario/factors (s/map-of string? number?))
+(s/def ::scenario (s/keys :req-un [:scenario/name :scenario/description :scenario/factors]))
+
+; -- TRANSIENT ----------------------
+(def nil-or-map (s/or :empty nil? :map map?))
+(s/def :transient/factor-edit-defaults nil-or-map)
+(s/def :transient/factor-active nil-or-map)
+(s/def :transient/factor-active-validation map?)
+(s/def :transient/scenario-edit-defaults nil-or-map)
+(s/def :transient/scenario-active nil-or-map)
+(s/def :transient/scenario-active-validation map?)
+
+; -- DEFAULT DB ---------------------
+(s/def :default-db/transient
+  (s/keys :req-un [:transient/factor-edit-defaults
+                   :transient/factor-active
+                   :transient/factor-active-validation
+                   :transient/scenario-edit-defaults
+                   :transient/scenario-active
+                   :transient/scenario-active-validation]))
+(s/def :default-db/factors (s/keys :req-un [:factors/all]))
+(s/def :default-db/scenarios (s/coll-of ::scenario :kind vector?))
+(s/def ::default-db (s/keys :req-un [:default-db/transient
+                                     :default-db/factors
+                                     :default-db/scenarios]))
+
 (def default-db
   {; Information that is used for procedures but that is
    ; not the resulting data that is the goal of the program
@@ -35,5 +62,10 @@
                ; Goal of duplication: avoid re-renders or
                ; having local state in the component
                :factor-active nil
-               :factor-active-validation {:is-valid nil}}
-   :factors {:all []}})
+               :factor-active-validation {:is-valid nil}
+               
+               :scenario-edit-defaults nil
+               :scenario-active nil
+               :scenario-active-validation {:is-valid nil}}
+   :factors {:all []}
+   :scenarios []})
