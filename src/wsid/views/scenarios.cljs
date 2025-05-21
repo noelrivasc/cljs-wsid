9ebb50bfb34d98352e35a572aca274c95b921d97
Noel Rivas <hola@noelr.dev>
2025-05-21 17:46:47 -0600

    Style scenarios edit form.

--- src/wsid/views/scenarios.cljs
+++ src/wsid/views/scenarios.cljs
@@ -93,40 +93,41 @@
   (let [scenario-edit-defaults (<sub [:scenario-edit-defaults])
         scenario-valid? (<sub [:scenario-active-is-valid])
         update-scenario (fn [el] (let [type (-> el .-target .-type)
-                                     property (-> el .-target .-name)
-                                     value (-> el .-target .-value)]
-                                 (evt> [:scenario-active-update property
-                                        (if (= type "number") (parse-long value) value)])))]
-    [:details {:open true}
-     [:summary "Edit Scenario"]
-     [:form.scenario-active-edit
-      [:label {:for "scenario-title"} "Title"
-       [:input.scenario-active-edit__input
-        {:defaultValue (:title scenario-edit-defaults)
-         :id "scenario-title"
-         :name "title"
-         :on-change update-scenario}]]
-      [:label {:for "scenario-description"} "Description"
-       [:textarea.scenario-active-edit__textarea
-        {:defaultValue (:description scenario-edit-defaults)
-         :id "scenario-description"
-         :name "description"
-         :on-change update-scenario}]]
+                                       property (-> el .-target .-name)
+                                       value (-> el .-target .-value)]
+                                   (evt> [:scenario-active-update property
+                                          (if (= type "number") (parse-long value) value)])))]
+    [:form.form
+     [:div.form__field
+      [:label.form__label {:for "scenario-title"} "Title"]
+      [:input.form__input
+       {:defaultValue (:title scenario-edit-defaults)
+        :id "scenario-title"
+        :name "title"
+        :on-change update-scenario}]]
 
-      [:div.scenario-form__actions
-       (if (:id scenario-edit-defaults)
-         [:input.scenario-form__actions__button.scenario-form__actions__button--delete
-          {:type "button"
-           :value "delete"
-           :on-click #(evt> [:scenario-active-delete])}]
-         nil)
-       [:input.scenario-form__actions__button.scenario-form__actions__button--cancel
-        {:type "button"
-         :value "cancel"
-         :on-click #(evt> [:scenario-active-cancel])}]
+     [:div.form__field
+      [:label.form__label {:for "scenario-description"} "Description"]
+      [:textarea.form__input.form__input--textarea
+       {:defaultValue (:description scenario-edit-defaults)
+        :id "scenario-description"
+        :name "description"
+        :on-change update-scenario}]]
 
-       [:input.scenario-form__actions__button.scenario-form__actions__button--save
-        (conj {:type "button"
-               :value "save"
-               :on-click #(evt> [:scenario-active-save])}
-              (when-not scenario-valid? {:disabled true}))]]]]))
+     [:div.form__actions
+      (if (:id scenario-edit-defaults)
+        [:input.button.button--danger
+         {:type "button"
+          :value "delete"
+          :on-click #(evt> [:scenario-active-delete])}]
+        nil)
+      [:input.button
+       {:type "button"
+        :value "cancel"
+        :on-click #(evt> [:scenario-active-cancel])}]
+
+      [:input.button.button--primary
+       (conj {:type "button"
+              :value "save"
+              :on-click #(evt> [:scenario-active-save])}
+             (when-not scenario-valid? {:disabled true}))]]]))
