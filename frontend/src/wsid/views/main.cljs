4809ae868792bbceba75e29bd8dcbfb1b9c98e3b
Noel Rivas <hola@noelr.dev>
2025-08-06 01:53:22 -0600

    Add a toolbar view, include the login button and improve styles.

--- frontend/src/wsid/views/main.cljs
+++ frontend/src/wsid/views/main.cljs
@@ -5,6 +5,7 @@
    [wsid.views.factors :refer [v-factors-panel v-factor-form]]
    [wsid.views.scenarios :refer [v-scenarios-panel v-scenario-form]]
    [wsid.views.decisions :refer [v-decision-panel]]
+   [wsid.views.user :refer [v-user-form]]
    [wsid.views.icons :as i]
    ["react-transition-group" :as rtg]))
 
@@ -20,46 +21,54 @@
       [:div.modal-container__header title]
       [:div.modal-container__main [content-fn]]]]]])
 
+(defn v-toolbar []
+  (let [db-is-dirty (<sub [:db-is-dirty])
+        icon-class (if db-is-dirty [:icon :opacity-100] [:icon :opacity-20])]
+    [:div.toolbar
+     [:div.toolbar__save-to-browser
+      [:button.button
+       {:on-click #(evt> ^:ls-compare [:save-to-local-storage])}
+       [:span.text "Save to Browser"]
+       [:span.button__icon {:class icon-class}
+        (i/get-icon i/edit :svg.icon--sm)]]]
+     [:div.toolbar__login
+      [:button.button
+       {:on-click #(evt> [:authenticate])}
+       [:span.text "Log in"]]]]))
+
 (defn v-main-panel []
-  (let [db-is-dirty (<sub [:db-is-dirty])]
-    [:main.main-container
-     [:div.title__wrapper
-      [:span.title__acronym "w.s.i.d"]
-      [:h1.title "What Should I Do?"]]
+  [:main.main-container
+   [:div.title__wrapper
+    [:span.title__acronym "w.s.i.d"]
+    [:h1.title "What Should I Do?"]]
 
-     [:div.work-area
-      [:div.work-area__file-tools
-       [:div.work-are__save-to-browser
-        [:input.button
-         {:type "button"
-          :value "Save to Browser"
-          :on-click #(evt> ^:ls-compare [:save-to-local-storage])}]
-        (when db-is-dirty
-          [:span.icon
-           (i/get-icon i/edit)])]]
+   [:div.work-area
+    [v-toolbar]
 
-      [:div.tool-container.tool-container--decision
-       [v-decision-panel]]
+    [:div.tool-container.tool-container--decision
+     [v-decision-panel]]
 
-      [:div.tool-container.tool-container--ai-help
-       [:input.form__input
-        {:type "text"
-         :placeholder "JWT Token"
-         :on-change #(evt> ^:ls-compare [:jwt-token-update (-> % .-target .-value)])}]
-       [:input.button {:type "button"
-                       :value "AI Help"
-                       :on-click #(evt> [:llm-fetch])}]]
+    [:div.tool-container.tool-container--ai-help
+     [:input.form__input
+      {:type "text"
+       :placeholder "JWT Token"
+       :on-change #(evt> ^:ls-compare [:jwt-token-update (-> % .-target .-value)])}]
+     [:input.button {:type "button"
+                     :value "AI Help"
+                     :on-click #(evt> [:llm-fetch])}]]
 
       ; SECTIONS
-      [:div.tool-container.tool-container--factors
-       [v-factors-panel]]
-      [:div.tool-container.tool-container--scenarios
-       [v-scenarios-panel]]]]))
+    [:div.tool-container.tool-container--factors
+     [v-factors-panel]]
+    [:div.tool-container.tool-container--scenarios
+     [v-scenarios-panel]]]])
     
 (defn v-main []
   (let [f-active (<sub [:factor-active-is-set])
-        s-active (<sub [:scenario-active-is-set])]
+        s-active (<sub [:scenario-active-is-set])
+        u-active (<sub [:user-active-is-set])]
    [:div.wsid-app
     [v-main-panel]
     [v-modal-dialog f-active v-factor-form "Edit Factor"]
-    [v-modal-dialog s-active v-scenario-form "Edit Scenario"]]))
+    [v-modal-dialog s-active v-scenario-form "Edit Scenario"]
+    [v-modal-dialog u-active v-user-form "Log In"]]))
